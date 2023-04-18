4864d6aba7354d7f7bd19c4d90607a7ca3e210bf
romainjysch <rom.jys@outlook.com>
2023-04-18 14:13:49 +0200

    feat(projet-cafheg-rfj): Préparation BDD

---
+++ src/test/java/ch/hearc/cafheg/business/allocations/AllocationStepDefinitions.java
@@ -0,0 +1,11 @@
+package ch.hearc.cafheg.business.allocations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class AllocationStepDefinitions {
+}
---
+++ src/test/java/ch/hearc/cafheg/business/allocations/RunAllocationServiceTest.java
@@ -0,0 +1,7 @@
+package ch.hearc.cafheg.business.allocations;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class RunAllocationServiceTest { }
