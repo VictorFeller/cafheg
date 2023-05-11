bc8dc28558b694bf185cb9c9548eec98ff880241
robinrolle <robinrolle1748@gmail.com>
2023-05-11 20:33:42 +0200

    refactor ajout des log errors pour exceptions SQL et log debug pour sout

--- src/main/java/ch/hearc/cafheg/infrastructure/persistance/EnfantMapper.java
+++ src/main/java/ch/hearc/cafheg/infrastructure/persistance/EnfantMapper.java
@@ -15,17 +15,18 @@ public class EnfantMapper extends Mapper {
 
   private static final Logger logger = LoggerFactory.getLogger(EnfantMapper.class);
   public Enfant findById(long id) {
-    System.out.println("Recherche d'un enfant par son id " + id);
+    logger.debug("Recherche d'un enfant par son id " + id);
     Connection connection = activeJDBCConnection();
     try {
       PreparedStatement preparedStatement = connection.prepareStatement(QUERY_FIND_ENFANT_BY_ID);
       preparedStatement.setLong(1, id);
       ResultSet resultSet = preparedStatement.executeQuery();
-      System.out.println("resultSet#next");
+      logger.debug("resultSet#next");
       resultSet.next();
       return new Enfant(new NoAVS(resultSet.getString(1)),
           resultSet.getString(2), resultSet.getString(3));
     } catch (SQLException e) {
+      logger.error("SQL excpetion : ",e);
       throw new RuntimeException(e);
     }
   }
