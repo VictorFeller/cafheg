b3af0089819c27b1d93863f2a2992a29209723e2
robinrolle <robinrolle1748@gmail.com>
2023-05-11 20:46:34 +0200

    ajout des autres cantons

--- src/main/java/ch/hearc/cafheg/business/allocations/Canton.java
+++ src/main/java/ch/hearc/cafheg/business/allocations/Canton.java
@@ -3,13 +3,32 @@ package ch.hearc.cafheg.business.allocations;
 import java.util.stream.Stream;
 
 public enum Canton {
-  NE,
-  BE,
-  FR,
-  GE,
-  SH;
-
-  // TODO: Ajouter les autres cantons..
+  AG, // Argovie (Aargau)
+  AI, // Appenzell Rhodes-Intérieures (Appenzell Innerrhoden)
+  AR, // Appenzell Rhodes-Extérieures (Appenzell Ausserrhoden)
+  BE, // Berne (Bern)
+  BL, // Bâle-Campagne (Basel-Landschaft)
+  BS, // Bâle-Ville (Basel-Stadt)
+  FR, // Fribourg (Freiburg)
+  GE, // Genève (Genf)
+  GL, // Glaris (Glarus)
+  GR, // Grisons (Graubünden)
+  JU, // Jura
+  LU, // Lucerne
+  NE, // Neuchâtel
+  NW, // Nidwald (Nidwalden)
+  OW, // Obwald (Obwalden)
+  SG, // Saint-Gall (St. Gallen)
+  SH, // Schaffhouse (Schaffhausen)
+  SO, // Soleure (Solothurn)
+  SZ, // Schwytz
+  TG, // Thurgovie (Thurgau)
+  TI, // Tessin (Ticino)
+  UR, // Uri
+  VD, // Vaud
+  VS, // Valais (Wallis)
+  ZG, // Zoug (Zug)
+  ZH;  // Zurich (Zürich)
 
   public static Canton fromValue(String value) {
     return Stream.of(Canton.values())
