17dc7a0d0889d3bb83161698fb1a3a9b460e707a
Romain Jysch <rjh-network@pm.me>
2023-05-25 18:47:27 +0200

    feature(rfj): First test

---
+++ src/integration-test/java/ch/hearc/cafheg/business/allocations/MyTestsIT.java
@@ -0,0 +1,13 @@
+package ch.hearc.cafheg.business.allocations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTestsIT {
+
+    @Test
+    void firstTest_Given1ShouldBe1() {
+        assertEquals(1, 1);
+    }
+}
